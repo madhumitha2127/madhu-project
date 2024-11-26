cb22ff63be80c97ed76244097f6b9d3881e8afa6
madhumitha203617 <24uad153madhu@kgkite.ac.in>
2024-11-26 11:51:21 +0200

    commit 3rd

---
+++ hack3.py
@@ -0,0 +1,31 @@
+#types of user-defined functions
+#function with arguments and return values
+def add(num1,num2):
+    print(num1+num2)
+    return add
+print("Addition of two numbers")   
+add(2,3) 
+#function without arguments and return values
+def sub():
+    num1=int(input("Enter a value1:"))
+    num2=int(input("Enter a value2:"))   
+    print(num1-num2)
+print("subraction of two numbers")    
+sub()
+#function with arguments and without return values
+def mul(num1,num2):
+    num3=num1*num2
+    print(num3)
+print("Multiply of two numbers")
+num1=int(input("Enter a value1:"))
+num2=int(input("Enter a value2:"))
+
+mul(num1,num2)
+#function without arguments and with return values
+def div():
+    num1=int(input("Enter a value1:"))
+    num2=int(input("Enter a value2:"))
+    print(num1/num2)
+    return div
+print("Division of two numbers")    
+div()
